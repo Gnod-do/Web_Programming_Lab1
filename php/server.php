53f2b2c88af839a64507fb3502b3d3a0c040cdd5
Gnod-do <gnodinrussia@gmail.com>
2023-09-24 21:35:27 +0300

    chore: change funtion check

--- php/server.php
+++ php/server.php
@@ -5,16 +5,18 @@ $start = microtime(true);
 $x = (float)$_POST['x'];
 $y = (float)$_POST['y'];
 $r = (float)$_POST['r'];
-function check($x, $y, $r)
-{
-    if ((($x >= 0) && ($x <= $r/2) && ($y >= 0) && ($y <= $r/2) && (-$x + $r/2 >= $y)) || 
-    (($x >= -$r/2) && ($x <= 0) && ($y >= 0) && ($y <= $r/2) && ($x*$x + $y*$y <= $r*$r/4)) ||
-    (($x >= -$r/2) && ($x <= 0) && ($y >= -$r) && ($y <= 0))) {
+function check($x, $y, $r) {
+    $condition1 = ($x >= 0) && ($x <= $r/2) && ($y >= 0) && ($y <= $r/2) && (-$x + $r/2 >= $y);
+    $condition2 = ($x >= -$r/2) && ($x <= 0) && ($y >= 0) && ($y <= $r/2) && ($x*$x + $y*$y <= $r*$r/4);
+    $condition3 = ($x >= -$r/2) && ($x <= 0) && ($y >= -$r) && ($y <= 0);
+
+    if ($condition1 || $condition2 || $condition3) {
         return "<span style='color: #7FFF5C'>True</span>";
     } else {
         return "<span style='color: #FF47A0'>False</span>";
     }
 }
+
 
 $result = check($x,$y,$r);
 $now = date("H:i:s");
